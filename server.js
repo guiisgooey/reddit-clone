84d0cbfb63f803a0f008b9e2f0f237827817b7b4
guiisgooey <49354894+guiisgooey@users.noreply.github.com>
2021-04-13 18:51:38 -0400

    did stuff

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+const express = require("express");
+
+// App Setup
+const app = express();
+
+// Middleware
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  title = "";
+  text = "";
+  if (req.query.title) {
+    title = req.query.title;
+  }
+  if (req.query.text) {
+    text = req.query.text;
+  }
+
+  res.render("home");
+});
+
+app.listen(3000, () => {
+  console.log("Reddit Clone listening on port localhost:3000!");
+});
