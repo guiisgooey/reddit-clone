160dd9ba0bbf3e2041910102743c2fc0cf2e8461
guiisgooey <49354894+guiisgooey@users.noreply.github.com>
2021-04-29 20:14:53 -0400

    users can submit and save posts
    title

--- server.js
+++ server.js
@@ -2,9 +2,24 @@ const express = require("express");
 
 // App Setup
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Middleware
 const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+// Set db
+require("./data/reddit-db");
+
+require("./controllers/posts.js")(app);
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -22,6 +37,19 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.listen(3000, () => {
-  console.log("Reddit Clone listening on port localhost:3000!");
+app.get("/posts/new", (req, res) => {
+  title = "";
+  text = "";
+  if (req.query.title) {
+    title = req.query.title;
+  }
+  if (req.query.text) {
+    text = req.query.text;
+  }
+
+  res.render("posts-new");
+});
+
+app.listen(port, () => {
+  console.log(`Reddit Clone listening on port ${port}!`);
 });
