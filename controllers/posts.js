a13a7a764d4405eab1a4d0942154aaf8c2ea3e1d
guiisgooey <49354894+guiisgooey@users.noreply.github.com>
2021-05-06 19:59:58 -0400

    Authors for posts are visible

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,14 +1,18 @@
 const Post = require("../models/post");
-const mongoose = require("mongoose");
+const User = require("../models/user");
 
 module.exports = (app) => {
   // VIEW ALL
   app.get("/", (req, res) => {
     var currentUser = req.user;
+    // res.render('home', {});
+    console.log(req.cookies);
     Post.find({})
       .lean()
+      .populate("author")
       .then((posts) => {
         res.render("posts-index", { posts, currentUser });
+        // res.render('home', {});
       })
       .catch((err) => {
         console.log(err.message);
@@ -18,21 +22,36 @@ module.exports = (app) => {
   app.post("/new/post", (req, res) => {
     if (req.user) {
       var post = new Post(req.body);
-
-      post.save(function (err, post) {
-        return res.redirect(`/`);
-      });
+      post.author = req.user._id;
+      post
+        .save()
+        .then((post) => {
+          return User.findById(req.user._id);
+        })
+        .then((user) => {
+          user.posts.unshift(post);
+          user.save();
+          // REDIRECT TO THE NEW POST
+          res.redirect(`/posts/${post._id}`);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
     } else {
       return res.status(401); // UNAUTHORIZED
     }
   });
   // LOOK UP THE POST
   app.get("/posts/:id", function (req, res) {
+    var currentUser = req.user;
+    // LOOK UP THE POST
+
     Post.findById(req.params.id)
       .lean()
       .populate("comments")
+      .populate("author")
       .then((post) => {
-        res.render("posts-show", { post });
+        res.render("posts-show", { post, currentUser });
       })
       .catch((err) => {
         console.log(err.message);
@@ -40,10 +59,12 @@ module.exports = (app) => {
   });
   // SUBREDDIT
   app.get("/n/:subreddit", function (req, res) {
+    var currentUser = req.user;
     Post.find({ subreddit: req.params.subreddit })
       .lean()
+      .populate("author")
       .then((posts) => {
-        res.render("posts-index", { posts });
+        res.render("posts-index", { posts, currentUser });
       })
       .catch((err) => {
         console.log(err);
--- models/post.js
+++ models/post.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({
-  title: { type: String, required: true },
-  url: { type: String, required: true },
-  summary: { type: String, required: true },
-  subreddit: { type: String, required: true },
-  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
-});
+const PostSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    url: { type: String, required: true },
+    summary: { type: String, required: true },
+    subreddit: { type: String, required: true },
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: { createdAt: "created_at" } }
+);
 
 const Post = mongoose.model("Post", PostSchema);
 
